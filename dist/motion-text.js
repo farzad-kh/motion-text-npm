1078eade9024046824169991b50c5d656f175e7e
farzad kh <farzadkh900@gmail.com>
2025-06-11 00:29:16 +0330

    import dist file

---
+++ dist/motion-text.js
@@ -0,0 +1,5 @@
+import { jsx as t } from "react/jsx-runtime";
+const n = ({ children: o }) => /* @__PURE__ */ t("button", { style: { background: "red", color: "#fff" }, children: o });
+export {
+  n as Button
+};
